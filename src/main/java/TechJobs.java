eea69f086101e6b88911cf91e01c4a5713fdef4a
MichaelSHuerta <michaelhuerta1997@gmail.com>
2021-10-04 21:53:09 -0400

    Finihsed working on Assignment 1

--- src/main/java/TechJobs.java
+++ src/main/java/TechJobs.java
@@ -10,7 +10,7 @@ public class TechJobs {
 
     static Scanner in = new Scanner(System.in);
 
-    public static void main (String[] args) {
+    public static void main(String[] args) {
 
         // Initialize our field map with key/name pairs
         HashMap<String, String> columnChoices = new HashMap<>();
@@ -112,14 +112,28 @@ public class TechJobs {
                 validChoice = true;
             }
 
-        } while(!validChoice);
+        } while (!validChoice);
 
         return choiceKeys[choiceIdx];
     }
 
     // Print a list of jobs
     private static void printJobs(ArrayList<HashMap<String, String>> someJobs) {
+        if (someJobs.size() >= 1) {
+            for (int i = 0; i < someJobs.size(); i++) {
+                System.out.println("");
+                System.out.println("*****");
 
-        System.out.println("printJobs is not implemented yet");
+                for (java.util.Map.Entry<String, String> job : someJobs.get(i).entrySet()) {
+                    System.out.println(job.getKey() + ": "  + job.getValue());
+                }
+                System.out.println("*****");
+            }
+
+
+        }
+        else {
+            System.out.print("No Results");
+        }
     }
 }
